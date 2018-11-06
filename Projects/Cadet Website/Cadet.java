11af99d968dc9755c618403852d17f97e5b624de
robinsonrobertjames <35779522+robinsonrobertjames@users.noreply.github.com>
2018-11-05 21:06:42 -0500

    stuff

--- Projects/Cadet Website/Cadet.java
+++ Projects/Cadet Website/Cadet.java
@@ -1,3 +1,19 @@
 class Cadet {
 
+  protected int id;
+  protected String last;
+  protected String first;
+  protected int rank;
+  protected int[] ranks_held;
+  protected int asyear;
+  protected char flight;
+  protected String email;
+  protected int phone;
+  protected int current_position;
+  protected int[] positions_held;
+
+  public Cadet( int id, String last, String first, int rank, int asyear, char flight, String email, int phone, int current_position){
+    
+  }
+
 }
